c7ab9d31edccf7d85e18603f5811cada8b7d68fb
l <l@l.l>
2018-01-06 20:05:15 +0300

    Refactor project structure and add respawn zone

---
+++ Assets/Scripts/Actions/RespawnTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class RespawnTrigger : MonoBehaviour {
+    public GameObject respawn;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            collision.transform.position = respawn.transform.position;
+        }
+    }
+}
